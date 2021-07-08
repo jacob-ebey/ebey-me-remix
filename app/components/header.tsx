c12a52458a2c4055a331e8cca8bb33e5d5f79cf4
Jacob Ebey <jacob.ebey@live.com>
2021-07-07 21:58:18 -0700

    updated header style

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="flex flex-wrap max-w-2xl px-4 py-4 mx-auto mb-4">
-      <Link to="/" className="inline-flex items-center p-2 hover:bg-yellow-100">
+      <Link to="/" className="inline-flex items-center p-2 pl-0 hover:bg-yellow-100">
         <svg className="inline-block w-5 h-5 mr-3 bg-black">
           <path d="M0 0h19v19H0z" />
         </svg>
